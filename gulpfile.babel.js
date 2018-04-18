3ecb8944038587606994940a4f465f3ac0cb7219
Paul Kirby <paul@undergroundweblab.com>
2018-04-18 12:42:05 -0500

    Updated Foundation to most recent version

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,11 +1,15 @@
 'use strict';
 
-import plugins   from 'gulp-load-plugins';
-import yargs     from 'yargs';
-import browser   from 'browser-sync';
-import gulp      from 'gulp';
-import yaml      from 'js-yaml';
-import fs        from 'fs';
+import plugins       from 'gulp-load-plugins';
+import yargs         from 'yargs';
+import browser       from 'browser-sync';
+import gulp          from 'gulp';
+import rimraf        from 'rimraf';
+import yaml          from 'js-yaml';
+import fs            from 'fs';
+import webpackStream from 'webpack-stream';
+import webpack2      from 'webpack';
+import named         from 'vinyl-named';
 
 // Load all Gulp plugins into one variable
 const $ = plugins();
@@ -13,20 +17,34 @@ const $ = plugins();
 // Check for --production flag
 const PRODUCTION = !!(yargs.argv.production);
 
-// Load all settings from config.yml
-const {PATHS, PORT, PROXY, COMPATIBILITY, UNCSS_OPTIONS} = loadConfig();
+// Load settings from settings.yml
+const { COMPATIBILITY, PORT, PATHS } = loadConfig();
 
 function loadConfig() {
   let ymlFile = fs.readFileSync('config.yml', 'utf8');
   return yaml.load(ymlFile);
 }
 
-// Build the distribution files by running all of the below tasks
-gulp.task('build', gulp.parallel(sass, javascript, specificJS));
+// Build the "dist" folder by running all of the below tasks
+gulp.task('build',
+  gulp.series(clean, gulp.parallel(sass, javascript, copy)));
 
 // Build the site, run the server, and watch for file changes
 gulp.task('default',
   gulp.series('build', server, watch));
+
+// Delete the "dist" folder
+// This happens every time a build starts
+function clean(done) {
+  rimraf(PATHS.dist, done);
+}
+
+// Copy files out of the assets folder
+// This task skips over the "img", "js", and "scss" folders, which are parsed separately
+function copy() {
+  return gulp.src(PATHS.assets)
+    .pipe(gulp.dest(PATHS.dist + '/'));
+}
 
 // Compile Sass into CSS
 // In production, the CSS is compressed
@@ -40,50 +58,53 @@ function sass() {
     .pipe($.autoprefixer({
       browsers: COMPATIBILITY
     }))
-  // Comment in the pipe below to run UnCSS in production
-    .pipe($.if(PRODUCTION, $.uncss(UNCSS_OPTIONS)))
-    .pipe($.if(PRODUCTION, $.cssnano()))
+    // Comment in the pipe below to run UnCSS in production
+    .pipe($.if(PRODUCTION, $.cleanCss({ compatibility: 'ie9' })))
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
-    .pipe(gulp.dest(PATHS.dist + '/assets/css'))
+    .pipe(gulp.dest(PATHS.dist + '/css'))
     .pipe(browser.reload({ stream: true }));
 }
 
+let webpackConfig = {
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
 // Combine JavaScript into one file
 // In production, the file is minified
 function javascript() {
-  return gulp.src(PATHS.javascript)
+  return gulp.src(PATHS.entries)
+    .pipe(named())
     .pipe($.sourcemaps.init())
-    .pipe($.babel({ignore: ['what-input.js']}))
-    .pipe($.concat('app.js'))
+    .pipe(webpackStream(webpackConfig, webpack2))
     .pipe($.if(PRODUCTION, $.uglify()
       .on('error', e => { console.log(e); })
     ))
     .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
-    .pipe(gulp.dest(PATHS.dist + '/assets/js'));
-}
-
-function specificJS() {
-  return gulp.src(PATHS.specificJS)
-    .pipe($.sourcemaps.init())
-    .pipe($.babel({ignore: ['what-input.js']}))
-    .pipe($.if(PRODUCTION, $.uglify()
-      .on('error', e => { console.log(e); })
-    ))
-    .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
-    .pipe(gulp.dest(PATHS.dist + '/assets/js'));
+    .pipe(gulp.dest(PATHS.dist + '/js'));
 }
 
 // Start a server with BrowserSync to preview the site in
 function server(done) {
   browser.init({
-    port: PORT, proxy: PROXY
+    server: PATHS.dist, port: PORT
   });
   done();
 }
 
 // Watch for changes to static assets, pages, Sass, and JavaScript
 function watch() {
+  gulp.watch(PATHS.assets, copy);
+  gulp.watch('templates/**/*.html').on('all', browser.reload);
   gulp.watch('src/scss/**/*.scss').on('all', sass);
-  gulp.watch(PATHS.specificJS).on('all', gulp.series(specificJS, browser.reload));
   gulp.watch('src/js/**/*.js').on('all', gulp.series(javascript, browser.reload));
 }
